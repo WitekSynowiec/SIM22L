9873e78836aae2b0374ce601d21c56966acd573d
Witold Józef Synowiec <w.synowiec@stud.elka.pw.edu.pl>
2022-05-12 17:16:13 +0200

    init

---
+++ cmake-build-debug/CMakeFiles/SIM22L.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SIM22L.dir/main.cpp.o"
+  "SIM22L"
+  "SIM22L.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SIM22L.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/SIM22L.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for SIM22L.
+# This may be replaced when dependencies are built.
